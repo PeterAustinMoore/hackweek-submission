e152d3a38d3ecfa84ae7dc229f10e41014e43e4a
Peter Moore <peter.moore@socrata.com>
2017-10-12 17:16:16 -0400

    update success

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-//$url = "Location: https://peter.demo.socrata.com/oauth/authorize?client_id=M45QwgboTFjF2aPVUUfYs5mtt&response_type=code&redirect_uri=https://performance-ingress.herokuapp.com/success.php";
-//header($url);
-include("grid.php");
+$url = "Location: https://peter.demo.socrata.com/oauth/authorize?client_id=M45QwgboTFjF2aPVUUfYs5mtt&response_type=code&redirect_uri=https://performance-ingress.herokuapp.com/success.php";
+header($url);
 ?>

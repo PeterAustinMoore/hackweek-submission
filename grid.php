a0828f87b9541f9fdbfac4cf677bc9e165053d7a
Peter Moore <peter.moore@socrata.com>
2017-10-12 16:59:44 -0400

    update success

--- grid.php
+++ grid.php
@@ -5,12 +5,24 @@
   src="https://code.jquery.com/jquery-3.2.1.min.js"
   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
   crossorigin="anonymous"></script>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="assets/js/src/bootstrap-table.css">
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css">
 
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"></script>
+
+  <!-- Latest compiled and minified Locales -->
+  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/locale/bootstrap-table-zh-CN.min.js"></script>
+
+<link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet"/>
+<script src="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
+<script>
+
+</script>
 </head>
 <body>
   <table id="table"
+         data-toggle="table"
          data-toolbar="#toolbar"
          data-show-refresh="true"
          data-show-export="true"
@@ -34,12 +46,5 @@
            </tr>
          </thead>
   </table>
-
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="assets/js/src/bootstrap-table.js"></script>
-
-  <!-- Editable -->
-  <script src="//rawgit.com/vitalets/x-editable/master/dist/bootstrap3-editable/js/bootstrap-editable.js"></script>
-  <script src="assets/js/src/extensions/editable/bootstrap-table-editable.js"></script>
 </body>
 </html>

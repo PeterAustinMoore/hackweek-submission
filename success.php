778537b42a57487f449f974ae514854cb6ec5310
Peter Moore <peter.moore@socrata.com>
2017-10-12 14:43:12 -0400

    update success

--- success.php
+++ success.php
@@ -4,13 +4,12 @@ $s=getenv("client_id");
 $url="https://peter.demo.socrata.com/oauth/access_token?client_id=M45QwgboTFjF2aPVUUfYs5mtt&client_secret=".$s."&grant_type=authorization_code&redirect_uri=https://performance-ingress.herokuapp.com/success.php&code=".$code;
 $fields=array("empty"=>"");
 $ch = curl_init();
-echo $url;
 //set the url, number of POST vars, POST data
 curl_setopt($ch,CURLOPT_URL, $url);
 curl_setopt($ch,CURLOPT_POST,1);
 curl_setopt($ch,CURLOPT_POSTFIELDS,$fields);
 $result = curl_exec($ch);
-$access_token = json_decode($result);
-echo $access_token["access_token"];
-
+$data = json_decode($result, true);
+$access_token = $data->{"access_token"};
+print $access_token;
 ?>

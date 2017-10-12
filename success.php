bea69f196967c38aff914e9ef62c9f1a61b4cb32
Peter Moore <peter.moore@socrata.com>
2017-10-12 14:30:02 -0400

    update success

--- success.php
+++ success.php
@@ -7,7 +7,8 @@ $ch = curl_init();
 
 //set the url, number of POST vars, POST data
 curl_setopt($ch,CURLOPT_URL, $url);
-curl_setopt($ch,CURLOPT_POST,"");
+curl_setopt($ch,CURLOPT_POST,0);
+
 $result = curl_exec($ch);
 $access_token = json_decode($result);
 echo $access_token["access_token"];

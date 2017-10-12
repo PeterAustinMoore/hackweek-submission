cd9ebcc241e8e8e7d2847dfe549c34d9ff72b210
Peter Moore <peter.moore@socrata.com>
2017-10-12 14:01:55 -0400

    add env vars

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $e = getenv('client_id');
-echo $e;
+
 $url = "https://peter.demo.socrata.com/oauth/authorize?client_id=".$e."&response_type=code&redirect_uri=https://performance-ingress.herokuapp.com/success.php";
+echo $url;
 header('Location: '.$url);
 ?>

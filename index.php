b37a3f006111910798b22c78ef134d9b17b2947b
Peter Moore <peter.moore@socrata.com>
2017-10-12 14:05:07 -0400

    add env vars

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 $e = getenv('client_id');
-
 $url = "https://peter.demo.socrata.com/oauth/authorize?client_id=".$e."&response_type=code&redirect_uri=https://performance-ingress.herokuapp.com/success.php";
-echo $url;
 header('Location: '.$url);
 ?>
--- success.php
+++ success.php
@@ -1,10 +1,3 @@
 <?php
 echo "Hello";
-$url = "https://www.google.com";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, TRUE);
-curl_setopt($ch, CURLOPT_NOBODY, TRUE); // remove body
-$h = curl_exec($ch);
-var_dump($h);
 ?>

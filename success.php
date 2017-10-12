e54976ee5e53129af3adc1720d0acefdf965da2a
Peter Moore <peter.moore@socrata.com>
2017-10-12 15:00:58 -0400

    update success

--- success.php
+++ success.php
@@ -6,7 +6,7 @@ $url="https://peter.demo.socrata.com/oauth/access_token?client_id=M45QwgboTFjF2a
 
 function getAccessCode($u) {
   $c = curl_init();
-  echo "<script>console.log(".$u.")</script>";
+  echo "<script>console.log('".$u."')</script>";
   $fields=array("empty"=>"");
   //set the url, number of POST vars, POST data
   curl_setopt($c,CURLOPT_URL, $u);

fec1d2f025bd72b60185c1ceb187b8e5912c3b69
Peter Moore <peter.moore@socrata.com>
2017-11-14 06:16:07 -0800

    no sqlite :(

--- pages/superadmin/settings.php
+++ pages/superadmin/settings.php
@@ -1,28 +1,8 @@
 <?php
-class settings extends SQLite3
-{
-    function __construct()
-    {
-        $this->open('../../assets/db/settings.db');
-    }
-}
-
-$db = new settings();
-
-$result = $db->query('SELECT * FROM defaults');
-
-while($res = $result->fetchArray(SQLITE3_ASSOC)){
-  switch ($res['pagename']) {
-    case "users_db":
-      $users_db = $res["url"];
-    case "departments_db":
-      $departments_db = $res["url"];
-    case "entry_db":
-      $entry_db = $res["url"];
-    case "activity_db":
-      $activity_db = $res["url"];
-  }
-}
-$base_url = "https://".$res["domain"];
-$socrata_users_url = "https://api.us.socrata.com/api/catalog/v1/users?domain=".$res["domain"];
-?>
+$base_url = "https://peter.demo.socrata.com";
+$users_db = "https://peter.demo.socrata.com/resource/mnj2-zafk.json";
+$departments_db = "https://peter.demo.socrata.com/resource/6z67-xud9.json";
+$entry_db = "https://peter.demo.socrata.com/resource/6273-y3wu.json";
+$activity_db = "https://peter.demo.socrata.com/resource/ymsm-cbme.json";
+$socrata_users_url = "https://api.us.socrata.com/api/catalog/v1/users?domain=".str_replace("https://","",$base_url);
+ ?>

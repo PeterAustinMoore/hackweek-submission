38ed117eea491cfcbad5f773da7c7bb2325a4a60
Peter Moore <peter.moore@socrata.com>
2017-11-17 11:12:22 -0800

    add snu! and fix data adding bug

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <html>
 </head>
+<meta charset="utf-8" />
+<link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../../assets/img/favicon.png">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <style>
 html, body {
   background-image: url('assets/img/background/background.jpg');

4247ebe3d40176602ae00731860e997376bd8b19
Peter Moore <peter.moore@socrata.com>
2017-11-17 10:04:42 -0800

    make homepage not ugly

--- index.php
+++ index.php
@@ -1,9 +1,48 @@
+<html>
+</head>
+<style>
+html, body {
+  background-image: url('assets/img/background/background.jpg');
+  background-size: 100% 100%;
+  height: 100%;
+  width: 100%;
+  margin:0;
+  text-align: center;
+}
+#wrapper {
+  display: block;
+}
+.b {
+  display: inline-block;
+  padding-left: 50px;
+  padding-right: 50px;
+  height: 150px;
+  color:white;
+  text-align: center;
+  line-height: 150px;
 
-<h3>View site as a...</h3>
-<a href="pages/admin/settings.php">Site Admin</a><br>
-<a href="pages/departments/data.php">Department Admin</a><br>
-<a href="pages/entry/data.php">Data Enterer</a><br>
-
+  -moz-border-radius: 200px / 70px;
+  -webkit-border-radius: 200px / 70px;
+  border-radius: 200px / 70px;
+}
+.b:hover{
+  font-size: 20px;
+}
+h1 {
+  padding-top: 10%;
+  color:white;
+}
+</style>
+</head>
+<body>
+<h1>VIEW SITE AS A...</h1>
+<div id="wrapper">
+  <a href="pages/admin/settings.php"><div class="b">PERFORMANCE GURU</div></a>
+  <a href="pages/departments/data.php"><div class="b">PROGRAM LEADER</div></a>
+  <a href="pages/entry/data.php"><div class="b">DATA OWNER/WIZARD</div></a>
+</div>
+</body>
+</html>
 
 <?php
 //$url = "Location: https://peter.demo.socrata.com/oauth/authorize?client_id=M45QwgboTFjF2aPVUUfYs5mtt&response_type=code&redirect_uri=https://performance-ingress.herokuapp.com/success.php";
